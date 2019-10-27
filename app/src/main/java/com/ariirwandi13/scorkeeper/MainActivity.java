8d78637193a0838a59ee71e691fbcba4b844b790
thelapan <ariirwndi13@gmail.com>
2019-10-27 14:38:45 +0700

    update import

--- app/src/main/java/com/ariirwandi13/scorkeeper/MainActivity.java
+++ app/src/main/java/com/ariirwandi13/scorkeeper/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ariirwandi13.scorkeeper;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void addGoalForTeamA(View v) {
